c0f3043ced6b83babe60ad41b6c9f3c78631e8c6
javoh1rqaxramonov <ubotirov22@.com>
2024-05-28 19:57:12 +0500

    finished

---
+++ index.js
@@ -0,0 +1,41 @@
+// 4-masala
+// let n = +prompt("n");
+// let a =  +prompt("a");
+// let b = (n % a);
+// console.log(b);
+   
+// 5-masala
+// let n = +prompt("n");
+// let b = n * (n-2); 
+// console.log(b);
+
+// 8-masala
+// let b = +prompt("b");
+// let t =  +prompt("t");
+// let y = (b+t)/2;
+// console.log(y);
+
+// 2-misol
+// let celse = +prompt('celse');
+// console.log(celse+273.15);
+// console.log(celse*1.80+32);
+
+
+// 3-masala
+// let vali = +prompt("v");
+// let ali =  +prompt("a");
+// let bil =  +prompt("b");
+// let g = (bil-vali);
+// let k = (ali-vali);
+// console.log(g);
+// console.log(k);
+
+// 7-masala
+// let a = +prompt("a");
+// let s =  +prompt("s");
+// let y = (a-s);
+// let h = (y/2)
+// console.log(h);
+
+
+
